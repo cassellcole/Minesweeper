59aa6a58325a4476b24ca8b1c9316068894283fb
David Cole Cassell <DLCassell@student.fullsail.edu>
2022-06-07 19:20:11 -0700

    App Start Up Classes

--- cApp.h
+++ cApp.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "wx/wx.h"
-
+#include  "cMain.h"
 
 
 class cApp : public wxApp
@@ -8,8 +8,11 @@ class cApp : public wxApp
 public:
 	cApp();
 	~cApp();
+public:
+	virtual bool OnInit();
 
-
+private:
+	cMain* m_frame1 = nullptr;
 };
 
 
---
+++ cMain.cpp
@@ -0,0 +1,15 @@
+#include "cMain.h"
+
+
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper")
+{
+
+}
+
+cMain::~cMain()
+{
+
+}
+
+
