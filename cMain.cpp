804a80f8acbbaa8a7519ab14f2f8f008d1e0fe34
David Cole Cassell <DLCassell@student.fullsail.edu>
2022-06-07 19:53:14 -0700

    Created Grid Started Minesweeper

--- cMain.cpp
+++ cMain.cpp
@@ -1,17 +1,42 @@
 #include "cMain.h"
 
 
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+EVT_BUTTON(10001, OnButtonClicked)
+
+wxEND_EVENT_TABLE();
+
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper", wxPoint(30,30), wxSize(800, 600))
 {
-	m_btn1 = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150,50) );
-	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
-	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+	btn = new wxButton * [nFieldWidth * nFieldHeight];
+	wxGridSizer* grid = new wxGridSizer(nFieldHeight, nFieldHeight, 0, 0);
+
+	for (int x = 0; x < nFieldWidth; x++)
+	{
+		for (int y = 0; y < nFieldHeight; y++)
+		{
+			btn[y * nFieldWidth + x] = new wxButton(this, 10000 + (y * nFieldWidth + x));
+			grid->Add(btn[y * nFieldWidth + x], 1, wxEXPAND | wxALL);
+
+			btn[y * nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+			
+		}
+	}
+
+	this->SetSizer(grid);
+	grid->Layout();
 }
 
 cMain::~cMain()
 {
-
+	delete[]btn;
 }
 
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	int x = (evt.GetId() - 10000) % nFieldWidth;
+	int y = (evt.GetId() - 10000) / nFieldWidth;
 
+	evt.Skip();
+}
--- cMain.h
+++ cMain.h
@@ -7,9 +7,16 @@ public:
 	cMain();
 	~cMain();
 
-	wxButton* m_btn1 = nullptr;
-	wxTextCtrl* m_txt1 = nullptr;
-	wxListBox* m_list1 = nullptr;
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton** btn;
+
+	
+
+	void OnButtonClicked(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+
 
 };
 
